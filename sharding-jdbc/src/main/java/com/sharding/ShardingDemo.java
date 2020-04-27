2d2b20149aeac81afc2c013024d07a7ba8955be4
e <297761674@qq.com>
2020-04-27 16:39:08 +0800

    测试2

--- sharding-jdbc/src/main/java/com/sharding/ShardingDemo.java
+++ sharding-jdbc/src/main/java/com/sharding/ShardingDemo.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShardingDemo {
     public static void main(String[] args) {
-        System.out.println("----------------------");
+        System.out.println("=======================");
         SpringApplication.run(ShardingDemo.class, args);
     }
 }
